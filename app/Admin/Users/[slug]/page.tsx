87ba45d46962387794578c5d537a82a35e28aa3b
Okormoru praise <okormorupraisecode@gmail.com>
2025-05-09 10:02:03 +0100

    h

--- app/Admin/Users/[slug]/page.tsx
+++ app/Admin/Users/[slug]/page.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import {AllUserInfo, UserInfo} from "@/app/Types";
+import {AllUserInfo} from "@/app/Types";
 import SettingsForm from "./SettingsForm";
-import {GetAllUsers} from "@/app/Admin/Users/getAllUsers";
 import {getParticularUserInfo} from "@/app/Admin/Users/getParticularUserInfo";
 import Transactions from "@/app/Admin/Users/[slug]/Transactions";
 import ToggleOptions from "@/app/Admin/Users/[slug]/ToggleOptions";
 import DeleteUser from "@/app/Admin/Users/[slug]/DeleteUser";
 
 export async function generateStaticParams() {
-    const fetchUsers = await GetAllUsers()
-    const getAllUsers: { data: UserInfo[] } = await fetchUsers?.json();
+    // const fetchUsers = await GetAllUsers()
+    const getAllUsers = {data: [{userid: 1}, {userid: 1}]};
 
     return getAllUsers.data.map((data) => {
         return {
-            slug: data.userid.toString()
+            slug: data?.["userid"]?.toString()
         }
     })
 }
